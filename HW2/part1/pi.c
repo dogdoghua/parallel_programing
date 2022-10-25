60b14b30d6301699a6b2fec36c5848add9128e26
dogdoghua <a0912938624@gmail.com>
2022-10-25 17:29:10 +0800

    monteCarlo func

--- HW2/part1/pi.c
+++ HW2/part1/pi.c
@@ -5,16 +5,51 @@
 #include <math.h>
 #define ll long long 
 
+ll int point_sum = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
+
+void *monteCarlo(void *param){
+    srand(time(NULL));
+    ll int toss = *(int*) param;
+    ll point_per_thread=0;
+
+    for(int i=0; i<toss; i++){
+        pthread_mutex_lock(&lock);
+
+        double x = (double)rand()/(double)RAND_MAX;
+        double y = (double)rand()/(double)RAND_MAX;
+        double dis = x*x + y*y;
+
+        if(dis <= 1.0)
+            point_per_thread ++;
+
+
+        pthread_mutex_unlock(&lock);
+    }
+
+}
 
 int main(int argc, char *argv[]){       // argv: inputs from command line, argc: numbers of argument
 
-    srand(time(NULL));
-    
     // convert arguments to int or long long int
     int num_thread = atoi(argv[0]);
     ll int num_toss = atoi(argv[1]);
     ll int toss_per_thread = num_thread/num_toss;
+
+    pthread_t threads[num_thread];
+    // threadData t_data[num_thread];
+
+    for(int i=0; i<num_thread; i++){
+        t_data[i].toss_per_thread = toss_per_thread;
+        pthread_create(&threads[i], NULL, monteCarlo, (void *) &toss_per_thread);
+    }
+
+    for(int i=0; i<num_thread; i++)
+        pthread_join(threads[i]);
+
+
+    pthread_mutex_destroy(&lock);
 
     return 0;
 }
